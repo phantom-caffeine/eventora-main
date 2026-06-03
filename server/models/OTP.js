d09a7839dbfa974a3758869713355b8bd3ce5aa3
phantom-caffeine <keshavchhabra.lc@gmail.com>
2026-06-04 00:19:57 +0530

    commit 1

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -4,11 +4,9 @@ const otpSchema = new mongoose.Schema({
     email: { type: String, required: true },
     otp: { type: String, required: true },
     action: { type: String, enum: ['account_verification', 'event_booking'], required: true },
-    createdAt: { type: Date, default: Date.now, expires: 300 } // OTP expires in 5 minutes
+    createdAt: { type: Date, default: Date.now, expires: 300 }
 }, {
     collection: 'otps'
 });
-
-otpSchema.index({ createdAt: 1 }, { expireAfterSeconds: 300 });
 
 module.exports = mongoose.model('OTP', otpSchema);
